7ca9dcc7f0edd8aab87396a85e0fddbc99cc79a7
aert14 <aert141414@gmail.com>
2023-10-17 00:28:07 +0300

    hw_3_real

--- hw_3.py
+++ hw_3.py
@@ -1,11 +1,13 @@
 class CountVectorizer():
     """Преобразование текста в матрицу количества лексем."""
 
+
     def __init__(self: "CountVectorizer") -> None:
         """Конструктор класса CountVectorizer."""
-        self.sent_list = []
+        self.sent_list = [] # для хранения словарей с токенами предложений
 
-    def fit_transform(self: "CountVectorizer", corpus: list[str]) -> list[int]:
+
+    def fit_transform(self: "CountVectorizer", corpus: list[str]) -> list[list[int]]:
         """Возвращает матрицу количестве лексем, полученную из корпуса."""
         for sent_indx, sentence in enumerate(corpus):
             self.sent_list.append({})
@@ -14,23 +16,36 @@ class CountVectorizer():
                 self.sent_list[sent_indx] = \
                                 {i:0 for i in self.sent_list[sent_indx - 1]}
 
-
             for word in sentence.split(" "):
                 word_ = word.lower()
                 if word_ not in self.sent_list[sent_indx]:
-                    self.sent_list[sent_indx-1][word_] = 0 # для формата
-                    self.sent_list[sent_indx][word_] = 1
+                    self.sent_list[sent_indx][word_] = 1 # добавление токена
                 else:
                     self.sent_list[sent_indx][word_] += 1
+
+        # так как токены добавлялись при обработке каждого предложения
+        # последовательно, необходимо добавить в словари каждого предложения,
+        # исключая последнее, неучтённые токены(так как после последнего
+        # токены не добавлялись). Причём очевидно, что все такие токены
+        # должны быть с нулевыми значениями частоты появления.
+        for sent_indx in range(len(self.sent_list) - 2, -1, -1):
+            diff = {k : 0 for k in set(self.sent_list[sent_indx + 1]) - \
+                                    set(self.sent_list[sent_indx ])}
+            self.sent_list[sent_indx] = self.sent_list[sent_indx] | diff
         return self.count_matrix
+
+
     @property
     def feature_names(self: "CountVectorizer") -> list[str]:
         """Возвращает имена фичей."""
         return list(self.sent_list[0].keys())
+
+
     @property
     def count_matrix(self: "CountVectorizer") -> list[list[int]]:
-        """Возвращает матрицу количества лекцем."""
+        """Возвращает матрицу количества лексем."""
         return [list(i.values()) for i in self.sent_list]
+
 
     def get_feature_names(self: "CountVectorizer") -> list[str]:
         """Выдаёт имена фичей."""
