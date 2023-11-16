231121b99ee0e7cdd2aa822d2866b4a378072934
aert14 <93609363+aert14@users.noreply.github.com>
2023-11-17 01:43:27 +0300

    Update pizza_factory.py

--- final/pizza_factory.py
+++ final/pizza_factory.py
@@ -10,3 +10,5 @@ def create_pizza(pizza_name: str, size: str) -> object:
     """Создает пиццу."""
     pizza_class = pizza_classes[pizza_name.lower()]
     return pizza_class(size)
+if __name__ == "__main__":
+    pass
